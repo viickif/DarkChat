dcaa30516b5e3fa9e025e8dc34be3f3f5e9db32b
viickif <vicki.wfu@gmail.com>
2019-01-20 16:10:41 -0800

    completed working marketplace

--- src/marketplaceAPI/Marketplace.java
+++ src/marketplaceAPI/Marketplace.java
@@ -1,5 +1,131 @@
 package marketplaceAPI;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Marketplace {
+
+    private Set<Product> allProducts;
+
+    /**
+     * Initializes a new Marketplace
+     * @param products to add to Marketplace
+     */
+    public Marketplace(List<Product> products){
+        allProducts=new HashSet<>();
+        allProducts.addAll(products);
+
+    }
+
+    /**
+     * Initializes a new Marketplace
+     * @param products to add to Marketplace
+     */
+    public Marketplace(Set<Product> products){
+        allProducts=new HashSet<>();
+        allProducts.addAll(products);
+
+    }
+
+    /**
+     * Initializes a new empty Marketplace
+     *
+     */
+    public Marketplace(){
+        allProducts=new HashSet<>();
+    }
+
+    /**
+     * fetches all products in the marketplace that have
+     * inventory count > 0
+     * @return List of products in marketplace with inventory
+     * count > 0
+     */
+    public List<Product> fetchAllProducts(){
+        List<Product> nonEmptyProducts=new ArrayList<>();
+
+        for(Product product:allProducts){
+            if(!product.inventoryIsEmpty()){
+                nonEmptyProducts.add(product);
+            }
+        }
+
+        return nonEmptyProducts;
+    }
+
+    /**
+     * fetches the product with the specified name
+     * if it exists
+     * @param name of the product to fetch. Such product
+     *             must exist in the marketplace
+     * @return the product matching the specified name
+     */
+    public Product fetchOneProduct(String name) {
+        for(Product product:allProducts) {
+            if (product.getName().equals(name)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Determines if a product with the specified name exists in
+     * the marketplace
+     * @param name of the product to search for
+     * @return true if a product matching the specified name exists
+     * in the marketplace and false otherwise
+     */
+    public boolean containsProduct(String name){
+        for(Product product:allProducts){
+            if(product.getName().equals(name)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Adds a new product into the marketplace if no product
+     * with the same name currently exists in the markletplace
+     * @param newProduct to add to the marketplace. No product
+     *                   with the same name must currently exists
+     *                   in the markletplace
+     */
+    public void addProduct(Product newProduct){
+        allProducts.add(newProduct);
+    }
+
+
+    /**
+     * Determines if a product in the marketplace has
+     * available inventory
+     * @param name of product to check inventory of. It must
+     *             exist in the marketplacr
+     * @return true if the product inventory > 0 and false otherwise
+     */
+    public boolean inventoryIsEmpty(String name){
+        for(Product product:allProducts){
+            if(product.getName().equals(name)){
+                return product.inventoryIsEmpty();
+            }
+        }
+        return false;
+    }
+    /**Purchases a product in the marketplace
+     * @param name of product to purchase. Such product
+     *             must currently exist in the marketplace and
+     *             its inventory is not empty
+     *
+     */
+    public void purchaseProduct(String name){
+        for(Product product:allProducts){
+            if(product.getName().equals(name)&& !product.inventoryIsEmpty()){
+                product.purchaseProduct();
+            }
+        }
+    }
 
 }
--- src/marketplaceAPI/Product.java
+++ src/marketplaceAPI/Product.java
@@ -1,4 +1,30 @@
 package marketplaceAPI;
 
 public class Product {
+    private String name;
+    private int price;
+    private int inventory;
+
+    public Product(String name, int price, int inventory){
+        this.name=name;
+        this.price=price;
+        this.inventory=inventory;
+    }
+
+    public void purchaseProduct(){
+        inventory--;
+    }
+
+    public boolean inventoryIsEmpty(){
+        return inventory==0;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
 }
